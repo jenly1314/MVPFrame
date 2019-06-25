c7f970189b2c9f121ad5814b5b30344bc64b0679
jenly1314 <jenly1314@mail.com>
2019-06-25 15:35:28 +0800

    增加说明

--- lib/src/main/java/com/king/frame/mvp/fragment/LazyBindingFragment.java
+++ lib/src/main/java/com/king/frame/mvp/fragment/LazyBindingFragment.java
@@ -1,6 +1,9 @@
 package com.king.frame.mvp.fragment;
 
 import android.databinding.ViewDataBinding;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 
 import com.king.frame.mvp.base.BasePresenter;
 import com.king.frame.mvp.base.BaseView;
@@ -8,6 +11,16 @@ import com.king.frame.mvp.base.BindingFragment;
 
 /**
  * 懒加载BindingFragment
+ *
+ * 说明：需要使用懒加载的几种方式
+ *
+ * 1. 在{@link Fragment}与{@link ViewPager}一起使用时使用懒加载，通过调用{@link #setUserVisibleHint(boolean)} 来触发懒加载。
+ *
+ * 2. 在{@link Fragment}通过{@link FragmentTransaction}的{@link FragmentTransaction#show(Fragment)} 和 {@link FragmentTransaction#hide(Fragment)}
+ *    来控制显示时使用懒加载，通过调用{@link #onHiddenChanged(boolean)}来触发懒加载。因为初次通过{@link FragmentTransaction#show(Fragment)}并不会触发
+ *    {@link #onHiddenChanged(boolean)}，要规避这一点可以在{@link FragmentTransaction#add(int, Fragment)}后，先调用
+ *    {@link FragmentTransaction#hide(Fragment)}，然后再调用{@link FragmentTransaction#show(Fragment)}来触发{@link #onHiddenChanged(boolean)}
+ *
  * @author <a href="mailto:jenly1314@gmail.com">Jenly</a>
  */
 public abstract class LazyBindingFragment<V extends BaseView, P extends BasePresenter<V>,VDB extends ViewDataBinding> extends BindingFragment<V,P,VDB> {
@@ -28,15 +41,12 @@ public abstract class LazyBindingFragment<V extends BaseView, P extends BasePres
 
     @Override
     public void initData() {
-        onLazyLoad();
+        lazyLoad();
     }
 
     private void onVisible(){
         isVisible = true;
-        if(isLoad()){
-            onLazyLoad();
-        }
-
+        lazyLoad();
     }
 
 
@@ -58,20 +68,22 @@ public abstract class LazyBindingFragment<V extends BaseView, P extends BasePres
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
-        if(isVisibleToUser){
+        if(getUserVisibleHint()){
             onVisible();
         }else{
             onInvisible();
         }
     }
 
-    private boolean isLoad(){
+    /**
+     * 懒加载，触发{@link #onLazyLoad()}
+     */
+    private void lazyLoad(){
         if(isFirstLoad && isPrepared && isVisible){
+            //保证只加载一次
             isFirstLoad = false;
-            return true;
+            onLazyLoad();
         }
-
-        return false;
     }
 
     public abstract void onLazyLoad();
