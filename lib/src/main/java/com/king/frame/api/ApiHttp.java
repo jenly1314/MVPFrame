219500176b56eabe372515613a067c7a1564c2de
jenly1314 <jenly1314@mail.com>
2019-07-23 14:27:42 +0800

    CI

--- lib/src/main/java/com/king/frame/api/ApiHttp.java
+++ lib/src/main/java/com/king/frame/api/ApiHttp.java
@@ -7,10 +7,14 @@ import com.king.frame.util.SSLSocketFactoryUtils;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 
 import java.lang.Class;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Observable;
+import io.reactivex.Single;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -108,12 +112,23 @@ public class ApiHttp {
     }
 
     /**
-     * 传入Class 通过{@link Retrofit#create(Class)}获得对应的Class
+     * 传入Class 通过{@link Retrofit}获得对应的Retrofit service
      * @param service
      * @param <T>
      * @return
      */
     public <T> T getRetrofitService(@NonNull Class<T> service) {
+        //通过二次代理获得
+        return createWrapperService(service);
+    }
+
+    /**
+     * 传入Class 通过{@link Retrofit#create(Class)}获得对应的Class
+     * @param service
+     * @param <T>
+     * @return
+     */
+    private <T> T getService(@NonNull Class<T> service) {
         if(mRetrofitServiceCache == null){
             mRetrofitServiceCache = new HashMap<>();
         }
@@ -131,6 +146,44 @@ public class ApiHttp {
         }
 
         return retrofitService;
+    }
+
+
+    /**
+     * 二次动态代理 Retrofit优化  详情参见：https://zhuanlan.zhihu.com/p/40097338
+     * @param serviceClass
+     * @param <T>
+     * @return
+     */
+    private <T> T createWrapperService(@NonNull Class<T> serviceClass) {
+        return (T) Proxy.newProxyInstance(serviceClass.getClassLoader(),
+                new Class<?>[]{serviceClass}, (proxy, method, args) -> {
+                    if (method.getReturnType() == Observable.class) {
+                        // 如果方法返回值是 Observable 的话，则包一层再返回
+                        return Observable.defer(() -> {
+                            final T service = getService(serviceClass);
+                            // 执行真正的 Retrofit 动态代理的方法
+                            return ((Observable) getRetrofitMethod(service, method)
+                                    .invoke(service, args));
+                        });
+                    } else if (method.getReturnType() == Single.class) {
+                        // 如果方法返回值是 Single 的话，则包一层再返回
+                        return Single.defer(() -> {
+                            final T service = getService(serviceClass);
+                            // 执行真正的 Retrofit 动态代理的方法
+                            return ((Single) getRetrofitMethod(service, method)
+                                    .invoke(service, args));
+                        });
+                    }
+
+                    // 返回值不是 Observable 或 Single 的话不处理
+                    final T service = getService(serviceClass);
+                    return getRetrofitMethod(service, method).invoke(service, args);
+                });
+    }
+
+    private <T> Method getRetrofitMethod(T service, Method method) throws NoSuchMethodException {
+        return service.getClass().getMethod(method.getName(), method.getParameterTypes());
     }
 
 }
